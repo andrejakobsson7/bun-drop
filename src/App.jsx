0b72b3c952aba8bc6319f0112e5b1e747b634596
andrejakobsson7 <andre.jakobsson7@hotmail.com>
2024-05-21 16:59:44 +0200

    Initial navbar complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import Register from "./pages/Register";
+import Menu from "./pages/Menu";
+import Cart from "./pages/Cart";
 import "./App.css";
 
 function App() {
   return (
     <>
       <Router>
+        <Navbar />
         <Routes>
-          <Route></Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/Signin" element={<SignIn />}></Route>
+          <Route path="/Register" element={<Register />}></Route>
+          <Route path="/Menu" element={<Menu />}></Route>
+          <Route path="/Cart" element={<Cart />}></Route>
         </Routes>
       </Router>
     </>
