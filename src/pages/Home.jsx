1003fd8749c647a92a4982df01eb70b6b600ac49
andrejakobsson7 <andre.jakobsson7@hotmail.com>
2024-05-22 10:43:50 +0200

    Set up basic layout for best sellers on landing page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,18 @@ function Home() {
       <div id="home-hero-wrapper">
         <h1>Heavenly good burgers</h1>
       </div>
+      <div id="home-best-sellers-wrapper">
+        <div id="home-best-sellers-label-wrapper">
+          <em>Our bestsellers</em>
+        </div>
+        <div id="home-best-sellers">
+          <div className="favorite"></div>
+          <div className="favorite"></div>
+          <div className="favorite"></div>
+          <div className="favorite"></div>
+          <div className="favorite"></div>
+        </div>
+      </div>
     </div>
   );
 }
