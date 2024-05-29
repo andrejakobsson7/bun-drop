df16bf779fbdbba45ae8fb649895dc4d17859902
andrejakobsson7 <andre.jakobsson7@hotmail.com>
2024-05-29 10:16:01 +0200

    Cart page quantity complete

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,34 +1,74 @@
-import React from "react";
-function CartItem() {
+import React, { useState, useEffect } from "react";
+import useLocalStorage from "../hooks/useLocalStorage";
+import useSharedVariables from "../hooks/useSharedVariables";
+function CartItem(props) {
+  const [quantity, setQuantity] = useState(props.item.quantity);
+  const [totalPrice, setTotalPrice] = useState(props.item.totalPrice);
+  const [quantityOptions, setQuantityOptions] = useState([]);
+  const localStorageHandler = useLocalStorage();
+  const sharedVariablesHandler = useSharedVariables();
+
+  useEffect(() => {
+    let options = [];
+    for (
+      let i = sharedVariablesHandler.minimumQuantity;
+      i <= sharedVariablesHandler.maximumQuantity;
+      i++
+    ) {
+      options.push(
+        <option key={i} value={i}>
+          {i}
+        </option>
+      );
+    }
+    setQuantityOptions(options);
+  }, []);
+
+  async function handleQuantityChange(e) {
+    const enteredQuantity = parseInt(e.target.value);
+    setQuantity(enteredQuantity);
+    const fixedTotalPrice = parseFloat(
+      (enteredQuantity * props.item.price).toFixed(2)
+    );
+    setTotalPrice(fixedTotalPrice);
+    const items = await localStorageHandler.getLocalStorage("cartItems");
+    const itemToUpdateIndex = items.findIndex((i) => i.id === props.item.id);
+    items[itemToUpdateIndex].quantity = enteredQuantity;
+    items[itemToUpdateIndex].totalPrice = fixedTotalPrice;
+    await localStorageHandler.setLocalStorage("cartItems", items);
+    props.onQuantityChange();
+  }
   return (
     <div className="cart-item-container">
       <div className="cart-item-image-wrapper">
-        <img src="images/burger-5.png" />
+        <img src={props.item.image} />
         <div className="cart-item-delete-wrapper-sm">
-          <button className="remove-from-cart-btn">
-            <i className="bi bi-trash3-fill"></i>
+          <button className="remove-from-cart-btn" onClick={props.onItemDelete}>
+            <p className="bi bi-trash3-fill"></p>
           </button>
         </div>
       </div>
       <div className="cart-item-info-wrapper">
         <div className="cart-item-name-wrapper">
-          <h3>Classic cheeseburger</h3>
+          <h3>{props.item.title}</h3>
         </div>
         <div className="cart-item-quantity-wrapper">
           <label>Quantity</label>
-          <input className="cart-item-quantity-input" type="number" />
+          <select value={quantity} onChange={handleQuantityChange}>
+            {quantityOptions}
+          </select>
         </div>
         <div className="cart-item-price-wrapper">
           <label>Price</label>
-          <p>79 SEK</p>
+          <p>$ {props.item.price}</p>
         </div>
         <div className="cart-item-total-price-wrapper">
           <label>Total</label>
-          <p>79 SEK</p>
+          <p>$ {totalPrice}</p>
         </div>
         <div className="cart-item-delete-wrapper-lg">
-          <button className="remove-from-cart-btn">
-            <i className="bi bi-trash3-fill "></i>
+          <button className="remove-from-cart-btn" onClick={props.onItemDelete}>
+            <p className="bi bi-trash3-fill "></p>
           </button>
         </div>
       </div>
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,22 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PageLabel from "../components/PageLabel";
 import CartItem from "../components/CartItem";
+import useLocalStorage from "../hooks/useLocalStorage";
 function Cart() {
+  const [cart, setCart] = useState([]);
+  const localStorageHandler = useLocalStorage();
+  const [totalCartValue, setTotalCartValue] = useState(0);
+  useEffect(() => {
+    const getItemsInCart = async () => {
+      const items = await localStorageHandler.getLocalStorage("cartItems");
+      setCart(items);
+    };
+    getItemsInCart();
+  }, []);
+
+  useEffect(() => {
+    let cartTotal = 0;
+    cart.forEach((i) => {
+      cartTotal += parseFloat(i.totalPrice);
+    });
+    setTotalCartValue(cartTotal.toFixed(2));
+  }, [cart]);
+
+  async function handleQuantityChange() {
+    const items = await localStorageHandler.getLocalStorage("cartItems");
+    setCart(items);
+  }
+  function handleItemDelete() {
+    console.log("Deleting item, need to recalculate total...");
+  }
   return (
     <div id="cart-container">
       <div id="cart-label-wrapper">
         <PageLabel label="CART" />
       </div>
       <div id="cart-items-wrapper">
-        <CartItem />
-        <CartItem />
-        <CartItem />
-        <CartItem />
-        <CartItem />
-        <CartItem />
+        {cart.map((i) => (
+          <CartItem
+            key={i.id}
+            item={i}
+            onQuantityChange={handleQuantityChange}
+            onItemDelete={handleItemDelete}
+          />
+        ))}
         <div id="cart-total-wrapper">
           <h3>
-            Total <span className="cart-total">79</span> SEK
+            Total $ <span className="cart-total">{totalCartValue}</span>
           </h3>
           <button className="cart-continue-to-payment-btn">
             Continue to payment
